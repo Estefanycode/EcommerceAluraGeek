5cdbd964468c0bd4427ee9ced4c7dcf01bb8cd88
steffi <125609812+Estefanycode@users.noreply.github.com>
2023-08-09 21:52:58 -0600

    Delete assets directory

--- assets/services/user_services.js
+++
@@ -1,6 +0,0 @@
-// GET
-const usersList = () => fetch('http://localhost:3000/adminUser').then(response => response.json()).catch(error => console.log(error));
-
-export const userServices = {
-    usersList
-};
