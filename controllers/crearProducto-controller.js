5a8a404eb864f0682b1726dac5fd625d7ec16cdc
steffi <125609812+Estefanycode@users.noreply.github.com>
2023-08-09 21:53:58 -0600

    Add files via upload

---
+++ controllers/crearProducto-controller.js
@@ -0,0 +1,26 @@
+import {
+    clientServices
+} from "../services/product_services.js";
+
+const form = document.querySelector('[data-formAdd]');
+const urlInput = document.querySelector('[data-url]');
+const categoryInput = document.querySelector('[data-category]');
+const nameInput = document.querySelector('[data-name]');
+const priceInput = document.querySelector('[data-price]');
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const url = urlInput.value;
+    const category = categoryInput.value;
+    const name = nameInput.value;
+    const price = priceInput.value;
+    const id = ""; 
+
+    try {
+        await clientServices.createProduct(url, name, price, id, category);
+        window.location.href = "../screens/todosProductos.html";
+    } catch (error) {
+        console.log(error);
+    }
+});
